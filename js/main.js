8d4bd8dc5085c69172346d19a061f47f41e396a0
miskat433 <miskat433@gmail.com>
2022-08-22 09:43:46 +0600

    order list added

--- js/main.js
+++ js/main.js
@@ -23,3 +23,32 @@ for (let i = 0; i < allCard.length; i++) {
 
 
 
+// Select player Count start
+let count = 0
+for (let i = 0; i < allCard.length; i++) {
+    document.getElementById(`card-button-${i}`).addEventListener("click", function (event) {
+
+        if (count !== 5) {
+            const findButton = event.target
+            findButton.setAttribute("disabled", true);
+            findButton.style.backgroundColor = "gray";
+
+            const findPlayerName = event.target.parentNode.children[0].innerText;
+
+            const findOrderList = document.getElementById("orderList")
+            const createList = document.createElement("li")
+            count = count + 1;
+            createList.innerHTML = ` ${count}. ${findPlayerName} `
+            findOrderList.appendChild(createList)
+        }
+        else {
+            alert("Not more than five people can be selected")
+        }
+    })
+
+}
+
+// Select player Count end
+
+
+
